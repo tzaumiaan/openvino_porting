22be1922d0d0ace98c4d91b2814b49b895474827
Min-An Chao <minan.chao@tum.de>
2019-07-14 01:02:37 +0200

    add inception_resnet_v2 and FLOPs counter

---
+++ model_utils.py
@@ -0,0 +1,123 @@
+import numpy as np
+from torchvision import models, transforms
+import torch
+
+def count_param_size(model):
+  # print parameter size of model
+  n_params = np.sum([np.prod(p.size()) for p in model.parameters() if p.requires_grad])
+  return n_params
+
+def count_flops(model, target_size):
+  from thop import profile
+  model_input = torch.randn(1, 3, target_size, target_size)
+  flops, n_params = profile(model, inputs=(model_input, ), verbose=False)
+  return flops, n_params
+
+# model instance
+def get_model(model_name):
+  if model_name == 'inception_v3':
+    model = models.inception_v3(pretrained=True, transform_input=False)
+  elif model_name == 'inception_v4':
+    from model_zoo.inceptionv4 import inceptionv4
+    model = inceptionv4(pretrained='imagenet')
+  elif model_name == 'inception_resnet_v2':
+    from model_zoo.inceptionresnetv2 import inceptionresnetv2
+    model = inceptionresnetv2(pretrained='imagenet')
+  elif model_name == 'xception':
+    from model_zoo.xception import xception
+    model = xception(pretrained='imagenet')
+  elif model_name == 'resnet50':
+    model = models.resnet50(pretrained=True)
+  elif model_name == 'resnet101':
+    model = models.resnet101(pretrained=True)
+  elif model_name == 'resnet152':
+    model = models.resnet152(pretrained=True)
+  elif model_name == 'resnext50_32x4d':
+    model = models.resnext50_32x4d(pretrained=True)
+  elif model_name == 'resnext101_32x8d':
+    model = models.resnext101_32x8d(pretrained=True)
+  elif model_name == 'densenet121':
+    model = models.densenet121(pretrained=True)
+  elif model_name == 'densenet169':
+    model = models.densenet169(pretrained=True)
+  elif model_name == 'densenet201':
+    model = models.densenet201(pretrained=True)
+  elif model_name == 'densenet161':
+    model = models.densenet161(pretrained=True)
+  elif model_name == 'squeezenet1_0':
+    model = models.squeezenet1_0(pretrained=True)
+  elif model_name == 'squeezenet1_1':
+    model = models.squeezenet1_1(pretrained=True)
+  elif model_name == 'mobilenet_v2':
+    from model_zoo.mobilenet import mobilenet_v2
+    model = mobilenet_v2(pretrained=True)
+  elif model_name == 'shufflenet_v2_x1_0':
+    from model_zoo.shufflenetv2 import shufflenet_v2_x1_0
+    model = shufflenet_v2_x1_0(pretrained=True)
+  elif model_name == 'shufflenet_v2_x0_5':
+    from model_zoo.shufflenetv2 import shufflenet_v2_x0_5
+    model = shufflenet_v2_x0_5(pretrained=True)
+  elif model_name == 'nasnet_a_large':
+    # Pad layer not supported by OpenVINO
+    from model_zoo.nasnet import nasnetalarge
+    model = nasnetalarge(num_classes=1000, pretrained='imagenet')
+  elif model_name == 'nasnet_a_mobile':
+    # Pad layer not supported by OpenVINO
+    from model_zoo.nasnet_mobile import nasnetamobile
+    model = nasnetamobile(num_classes=1000, pretrained='imagenet')
+  elif model_name == 'efficientnet_b0':
+    from model_zoo.efficientnet_pytorch import EfficientNet
+    model = EfficientNet.from_pretrained('efficientnet-b0')
+  elif model_name == 'efficientnet_b1':
+    from model_zoo.efficientnet_pytorch import EfficientNet
+    model = EfficientNet.from_pretrained('efficientnet-b1')
+  elif model_name == 'efficientnet_b4':
+    from model_zoo.efficientnet_pytorch import EfficientNet
+    model = EfficientNet.from_pretrained('efficientnet-b4')
+  else:
+    raise ValueError('invalid model')
+  
+  # evaluation mode
+  model = model.eval()
+  
+  return model
+
+# transformation
+def get_trans(model_name):
+  # norm_mean and norm_std defined as RGB
+  imagenet_mean = [0.485, 0.456, 0.406]
+  imagenet_std = [0.229, 0.224, 0.225]
+  tf_slim_mean = [0.5, 0.5, 0.5]
+  tf_slim_std = [0.5, 0.5, 0.5]
+  if model_name.startswith('inception') or model_name.startswith('xception'):
+    target_size = 299
+    norm_mean = tf_slim_mean
+    norm_std = tf_slim_std
+  elif model_name == 'nasnet_a_large':
+    target_size = 331
+    norm_mean = tf_slim_mean
+    norm_std = tf_slim_std
+  elif model_name == 'nasnet_a_mobile':
+    target_size = 224
+    norm_mean = tf_slim_mean
+    norm_std = tf_slim_std
+  elif model_name.startswith('efficientnet'):
+    target_size = 240
+    norm_mean = imagenet_mean
+    norm_std = imagenet_std
+  else:
+    target_size = 224
+    norm_mean = imagenet_mean
+    norm_std = imagenet_std
+  print('target size = {}'.format(target_size))
+  print('normalization mean = {}, std = {}'.format(norm_mean, norm_std))
+    
+  # transformation
+  trans = transforms.Compose([
+    transforms.Resize(int(target_size/224*256)),
+    transforms.CenterCrop(target_size),
+    transforms.ToTensor(),
+    transforms.Normalize(norm_mean, norm_std)
+  ])
+
+  return target_size, trans
--- pytorch_onnx_openvino.py
+++ pytorch_onnx_openvino.py
@@ -1,5 +1,4 @@
 import torch
-from torchvision import models, transforms
 from PIL import Image
 import numpy as np
 import os
@@ -8,107 +7,7 @@ import onnxruntime
 from openvino.inference_engine import IENetwork, IEPlugin
 from datetime import datetime
 
-# model instance
-def get_model(model_name):
-  if model_name == 'inception_v3':
-    model = models.inception_v3(pretrained=True, transform_input=False)
-  elif model_name == 'inception_v4':
-    from model_zoo.inceptionv4 import inceptionv4
-    model = inceptionv4(pretrained='imagenet')
-  elif model_name == 'xception':
-    from model_zoo.xception import xception
-    model = xception(pretrained='imagenet')
-  elif model_name == 'resnet50':
-    model = models.resnet50(pretrained=True)
-  elif model_name == 'resnet101':
-    model = models.resnet101(pretrained=True)
-  elif model_name == 'resnet152':
-    model = models.resnet152(pretrained=True)
-  elif model_name == 'resnext50_32x4d':
-    model = models.resnext50_32x4d(pretrained=True)
-  elif model_name == 'resnext101_32x8d':
-    model = models.resnext101_32x8d(pretrained=True)
-  elif model_name == 'squeezenet1_0':
-    model = models.squeezenet1_0(pretrained=True)
-  elif model_name == 'squeezenet1_1':
-    model = models.squeezenet1_1(pretrained=True)
-  elif model_name == 'mobilenet_v2':
-    from model_zoo.mobilenet import mobilenet_v2
-    model = mobilenet_v2(pretrained=True)
-  elif model_name == 'shufflenet_v2_x1_0':
-    from model_zoo.shufflenetv2 import shufflenet_v2_x1_0
-    model = shufflenet_v2_x1_0(pretrained=True)
-  elif model_name == 'shufflenet_v2_x0_5':
-    from model_zoo.shufflenetv2 import shufflenet_v2_x0_5
-    model = shufflenet_v2_x0_5(pretrained=True)
-  elif model_name == 'nasnet_a_large':
-    # Pad layer not supported by OpenVINO
-    from model_zoo.nasnet import nasnetalarge
-    model = nasnetalarge(num_classes=1000, pretrained='imagenet')
-  elif model_name == 'nasnet_a_mobile':
-    # Pad layer not supported by OpenVINO
-    from model_zoo.nasnet_mobile import nasnetamobile
-    model = nasnetamobile(num_classes=1000, pretrained='imagenet')
-  elif model_name == 'efficientnet_b0':
-    from model_zoo.efficientnet_pytorch import EfficientNet
-    model = EfficientNet.from_pretrained('efficientnet-b0')
-  elif model_name == 'efficientnet_b1':
-    from model_zoo.efficientnet_pytorch import EfficientNet
-    model = EfficientNet.from_pretrained('efficientnet-b1')
-  elif model_name == 'efficientnet_b4':
-    from model_zoo.efficientnet_pytorch import EfficientNet
-    model = EfficientNet.from_pretrained('efficientnet-b4')
-  else:
-    raise ValueError('invalid model')
-  
-  # evaluation mode
-  model = model.eval()
-  
-  return model
-
-def print_param_size(model_name, model):
-  # print parameter size of model
-  n_params = np.sum([np.prod(p.size()) for p in model.parameters() if p.requires_grad])
-  print('{} has {}M trainable parameters'.format(model_name, n_params/1e6))
-
-def get_trans(model_name):
-  # norm_mean and norm_std defined as RGB
-  imagenet_mean = [0.485, 0.456, 0.406]
-  imagenet_std = [0.229, 0.224, 0.225]
-  tf_slim_mean = [0.5, 0.5, 0.5]
-  tf_slim_std = [0.5, 0.5, 0.5]
-  if model_name in ['inception_v3', 'inception_v4', 'xception']:
-    target_size = 299
-    norm_mean = tf_slim_mean
-    norm_std = tf_slim_std
-  elif model_name == 'nasnet_a_large':
-    target_size = 331
-    norm_mean = tf_slim_mean
-    norm_std = tf_slim_std
-  elif model_name == 'nasnet_a_mobile':
-    target_size = 224
-    norm_mean = tf_slim_mean
-    norm_std = tf_slim_std
-  elif model_name.startswith('efficientnet'):
-    target_size = 240
-    norm_mean = imagenet_mean
-    norm_std = imagenet_std
-  else:
-    target_size = 224
-    norm_mean = imagenet_mean
-    norm_std = imagenet_std
-  print('target size = {}'.format(target_size))
-  print('normalization mean = {}, std = {}'.format(norm_mean, norm_std))
-    
-  # transformation
-  trans = transforms.Compose([
-    transforms.Resize(int(target_size/224*256)),
-    transforms.CenterCrop(target_size),
-    transforms.ToTensor(),
-    transforms.Normalize(norm_mean, norm_std)
-  ])
-
-  return target_size, trans
+from model_utils import count_flops, get_model, get_trans
 
 def run_inference_pytorch(image_file, model_name, model, trans):
   from imagenet_labels import labelmap
@@ -265,8 +164,11 @@ if __name__=='__main__':
   # pytorch
   print('==== pytorch ====')
   model = get_model(args.model_name)
-  print_param_size(args.model_name, model)
   target_size, trans = get_trans(args.model_name)
+  flops, n_params = count_flops(model, target_size)
+  print('{} has {:.4f}M trainable parameters'.format(args.model_name, n_params/1e6))
+  print('{} has {:.4f}G FLOPs'.format(args.model_name, flops/1e9))
+
   run_inference_pytorch(args.image_file, args.model_name, model, trans)
   
   # onnx
